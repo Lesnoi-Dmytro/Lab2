fb2d13f2b86e1e16fec10a51d9fe68d19ccd1702
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2025-03-26 22:50:55 +0200

    changed routing

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import './style.css';
-import {start as startTask1} from './tasks/task1.js';
-import {start as startTask2} from './tasks/task2.js';
-import {start as startTask3} from './tasks/task3.js';
-import {start as startTask4} from './tasks/task4.js';
+import "./style.css";
+import { start as startTask1 } from "./tasks/task1.js";
+import { start as startTask2 } from "./tasks/task2.js";
+import { start as startTask3 } from "./tasks/task3.js";
+import { start as startTask4 } from "./tasks/task4.js";
 
-if (window.location.hash === '#/task1') {
-  startTask1()
-} else if (window.location.hash === '#/task2') {
-  startTask2()
-} else if (window.location.hash === '#/task3') {
-  startTask3()
-} else if (window.location.hash === '#/task4') {
-  startTask4()
+if (window.location.hash === "#/task1") {
+  startTask1();
+} else if (window.location.hash === "#/task2") {
+  startTask2();
+} else if (window.location.hash === "#/task3") {
+  startTask3();
+} else if (window.location.hash === "#/task4") {
+  startTask4();
 }
+
+window.addEventListener("hashchange", () => window.location.reload());
