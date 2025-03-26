b741ad8fa750e68aff598aa402a9868600d03095
Lesnoi Dmytro <dmytrolesnoi@gmail.com>
2025-03-26 21:54:49 +0200

    updated rounting

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import {start as startTask2} from './tasks/task2.js';
 import {start as startTask3} from './tasks/task3.js';
 import {start as startTask4} from './tasks/task4.js';
 
-if (window.location.pathname === '/#/task1') {
+if (window.location.hash === '#/task1') {
   startTask1()
-} else if (window.location.pathname === '/#/task2') {
+} else if (window.location.hash === '#/task2') {
   startTask2()
-} else if (window.location.pathname === '/#/task3') {
+} else if (window.location.hash === '#/task3') {
   startTask3()
-} else if (window.location.pathname === '/#/task4') {
+} else if (window.location.hash === '#/task4') {
   startTask4()
 }
